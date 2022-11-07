8fac7ce8f2383317f1070986097f386a6aa5a4f5
ConstDobrovol <constantin.dobrovol@mail.ru>
2022-11-08 01:55:25 +0300

    Последняя работа

--- Домашняя работа 1/VEC_2/src/Vectors.java
+++ Домашняя работа 1/VEC_2/src/Vectors.java
@@ -1,91 +1,52 @@
-import java.util.Arrays;
-
-import static javax.swing.UIManager.get;
-
 public class Vectors {
-    public static void main(String[] args) {
-        Vector[] vectors = Vector.massRand(3);
-
-        System.out.println(vectors[0]);
-        System.out.println(vectors[1]);
-        System.out.println(vectors[0].length());
-        System.out.println(vectors[0].scalarPr(vectors[1]));
-        System.out.println(vectors[0].crossProduct(vectors[1]));
-        System.out.println(vectors[0].cos(vectors[1]));
-        System.out.println(vectors[0].plus(vectors[1]));
-        System.out.println(vectors[0].minus(vectors[1]));
-    }
-
-    public static class Vector { //создаем класс вектор
-        private double x;//объявляем координаты вектора
-        private double y;
-        private double z;
-
-        // С тремя параметрами
-        public Vector(double x, double y, double z) {
+    public static class Vector { // class Vector
+        private double x,y,z;
+        public Vector(double x, double y, double z) {//конструктор с параметрами в виде списка координат x, y, z
             this.x = x;
             this.y = y;
             this.z = z;
         }
-
-        // метод, вычисляющий длину вектора
-        public double length() {
+        public double len() {// длина вектора
             return Math.sqrt(x * x + y * y + z * z);
         }
-
-        // метод, вычисляющий скалярное произведение
-        public double scalarPr(Vector vector) {
+        public double scalar_pro(Vector vector) {// скалярное произведение
             return x * vector.x + y * vector.y + z * vector.z;
         }
-
-        // метод, вычисляющий векторное произведение с другим вектором
-        public Vector crossProduct(Vector vector) {
+        public Vector vector_pro(Vector vector) {// векторное произведение
             return new Vector(
                     y * vector.z - z * vector.y,
                     z * vector.x - x * vector.z,
                     x * vector.y - y * vector.x);
         }
-
-        // Косинус между двумя векторами
-        public double cos(Vector vector) {
-
-
-            return scalarPr(vector) / (length() * vector.length());
+        public double cos(Vector vector) {//угол между векторами (или косинус угла):
+            return scalar_pro(vector) / (len() * vector.len());
         }
-
-        //метод для суммы
-        public Vector plus(Vector vector) {
-            return new Vector( //возвращается сложенный вектор
-                    x + vector.x,
-                    y + vector.y,
-                    z + vector.z
-            );
+        public Vector plus(Vector vector) {//метод для суммы
+            return new Vector( x + vector.x,y + vector.y,z + vector.z);
         }
-        //метод для разницы
-        public Vector minus(Vector vector) {
-            return new Vector(//возвращается вектор разницы
-                    x - vector.x,
-                    y - vector.y,
-                    z - vector.z
-            );
+        public Vector minus(Vector vector) {//метод для разницы
+            return new Vector(x - vector.x,y - vector.y,z - vector.z);
         }
-        //статический метод, который принимает целое число N, и возвращает массив случайных векторов размером N
-        public static Vector[] massRand(int n){
-            Vector[] vectors = new Vector[n];//создается новый массив размером n
-            for(int i =0; i < n; i++){//проходимся по массиву
-                vectors[i] = new Vector(Math.random(), Math.random(), Math.random());// проходя по массиву заполняем его случайными координатами векторов
+        public static Vector[] massRand(int n){//статический метод
+            Vector[] vectors = new Vector[n];
+            for(int i = 0; i < n; i++){//проходимся по массиву
+                vectors[i] = new Vector(Math.random(), Math.random(), Math.random());
             }
             return vectors;
         }
-
         public String toString() {
-            return "Вектор {" +
-                    "x=" + x +
-                    ", y=" + y +
-                    ", z=" + z +
-                    '}';
+            return "Вектор{" +"x=" + x +", y=" + y +", z=" + z +'}';
         }
-
-
+    }
+    public static void main(String[] args) {
+        Vector[] vectors = Vector.massRand(3);
+        System.out.println(vectors[0]);
+        System.out.println(vectors[1]);
+        System.out.println(vectors[0].len());
+        System.out.println(vectors[0].scalar_pro(vectors[1]));
+        System.out.println(vectors[0].vector_pro(vectors[1]));
+        System.out.println(vectors[0].cos(vectors[1]));
+        System.out.println(vectors[0].plus(vectors[1]));
+        System.out.println(vectors[0].minus(vectors[1]));
     }
 }
