a01299f6b8cf8fcdb0d6c5ff40ceef93c5fb54bc
ConstDobrovol <constantin.dobrovol@mail.ru>
2022-12-24 08:05:01 +0300

    dd

---
+++ Домашняя работа 5 (3 декабря )/HM_3/src/HW_1/recursion.java
@@ -0,0 +1,19 @@
+package HW_1;
+public class recursion {
+    public static void main(String[] args) {
+        System.out.println(numbers(15));
+    }
+
+    public static StringBuilder numbers(int number) {
+        StringBuilder str = new StringBuilder();
+        if (number > 1) {
+            str.append(numbers(number-1));
+            str.append(" ");
+            str.append(number);
+        } else{
+            str.append(1);
+        }
+        return str;
+
+    }
+}
